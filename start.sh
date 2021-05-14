299553375e2f99a0975c8bdfe204bca273858d4f
flobit <flobit@qq.com>
2021-05-14 11:15:29 +0800

    调整启动命令

--- start.sh
+++ start.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-$KAFKA_HOME/bin/kafka-server-start.sh $KAFKA_HOME/config/server.properties --override "`env|xargs`"
+env_params="";for i in `env`;do env_params="$env_params --override $i";done;
+command="$KAFKA_HOME/bin/kafka-server-start.sh $KAFKA_HOME/config/server.properties $env_params"
+$command
